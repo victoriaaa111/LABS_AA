4efc24381f63355f8c992218f8ddb1675e0308b8
victoriaaa111 <victoria.mutruc@gmail.com>
2025-02-18 22:36:21 +0200

    Add dynamic and array methods

--- LAB1/fibonacci.py
+++ LAB1/fibonacci.py
@@ -15,3 +15,21 @@ def fibonacci_recursive(n):
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
 
+# 2. dynamic method
+def fibonacci_dynamic(n):
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[n]
+
+
+# 3. array method
+def fibonacci_array(n):
+    if n <= 0:
+        return "Incorrect Output"
+    data = [0, 1]
+    if n > 2:
+        for i in range(2, n):
+            data.append(data[i - 1] + data[i - 2])
+    return data[n - 1]
+
