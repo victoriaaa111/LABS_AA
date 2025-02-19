1410d1b66a8c7a76f5acf11472247048d1ea734d
victoriaaa111 <victoria.mutruc@gmail.com>
2025-02-19 22:56:10 +0200

    Add table, plots and replace array method with backtracking with memorization

--- LAB1/fibonacci.py
+++ LAB1/fibonacci.py
@@ -1,57 +1,58 @@
 import time
 import matplotlib.pyplot as plt
 from decimal import Decimal, Context, ROUND_HALF_EVEN
+from tabulate import tabulate
+import sys
+
+sys.setrecursionlimit(16000)
 
 
 # 1. recursive method
-def fibonacci_recursive(n):
-    if n <= 0:
+def fibonacci_recursive(nr):
+    if nr <= 0:
         print("Incorrect input")
-    elif n == 1:
+    elif nr == 1:
         return 0
-    elif n == 2:
+    elif nr == 2:
         return 1
     else:
-        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+        return fibonacci_recursive(nr - 1) + fibonacci_recursive(nr - 2)
 
 
 # 2. dynamic method
-def fibonacci_dynamic(n):
+def fibonacci_dynamic(nr):
     fib = [0, 1]
-    for i in range(2, n + 1):
+    for i in range(2, nr + 1):
         fib.append(fib[i - 1] + fib[i - 2])
-    return fib[n]
+    return fib[nr]
 
 
-# 3. array method
-def fibonacci_array(n):
-    if n <= 0:
-        return "Incorrect Output"
-    data = [0, 1]
-    if n > 2:
-        for i in range(2, n):
-            data.append(data[i - 1] + data[i - 2])
-    return data[n - 1]
+# 3. backtracking with memorization method
+def fibonacci_backtracking(nr, memo={}):
+    if nr <= 0:
+        return 0
+    elif nr == 1:
+        return 1
+    elif nr in memo:
+        return memo[nr]
+    memo[nr] = fibonacci_backtracking(nr - 1, memo) + fibonacci_backtracking(nr - 2, memo)
+    return memo[nr]
 
 
 # 4. matrix method
-def fibonacci_matrix(n):
+def fibonacci_matrix(nr):
     fib = [[1, 1], [1, 0]]
-    if n == 0:
+    if nr == 0:
         return 0
-    power(fib, n - 1)
+    power(fib, nr - 1)
     return fib[0][0]
 
 
 def multiply(F, M):
-    x = (F[0][0] * M[0][0] +
-        F[0][1] * M[1][0])
-    y = (F[0][0] * M[0][1] +
-        F[0][1] * M[1][1])
-    z = (F[1][0] * M[0][0] +
-        F[1][1] * M[1][0])
-    w = (F[1][0] * M[0][1] +
-        F[1][1] * M[1][1])
+    x = (F[0][0] * M[0][0] + F[0][1] * M[1][0])
+    y = (F[0][0] * M[0][1] + F[0][1] * M[1][1])
+    z = (F[1][0] * M[0][0] + F[1][1] * M[1][0])
+    w = (F[1][0] * M[0][1] + F[1][1] * M[1][1])
 
     F[0][0] = x
     F[0][1] = y
@@ -59,31 +60,26 @@ def multiply(F, M):
     F[1][1] = w
 
 
-def power(F, n):
+def power(F, nr):
     M = [[1, 1], [1, 0]]
-    for i in range(2, n + 1):
+    for i in range(2, nr + 1):
         multiply(F, M)
 
 
 # 5. Binet Formula
-def fibonacci_binet(n):
+def fibonacci_binet(nr):
     ctx = Context(prec=60, rounding=ROUND_HALF_EVEN)
     phi = Decimal((1 + Decimal(5**(1/2))))
     phi2 = Decimal((1 - Decimal(5**(1/2))))
-    return int((ctx.power(phi, Decimal(n)) - ctx.power(phi2,Decimal(n))) / (2**n * Decimal(5**(1/2))))
+    return int((ctx.power(phi, Decimal(nr)) - ctx.power(phi2, Decimal(nr))) / (2**nr * Decimal(5**(1/2))))
 
 
-# 6. backtracking method
-def fibonacci_backtracking(n, memo={}):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n in memo:
-        return memo[n]
-    else:
-        memo[n] = fibonacci_backtracking(n - 1) + fibonacci_backtracking(n - 2)
-        return memo[n]
+# 6. iterative method
+def fibonacci_iterative(nr):
+    a, b = 0, 1
+    for _ in range(nr):
+        a, b = b, a + b
+    return a
 
 
 # measure time
@@ -94,11 +90,61 @@ def measure_time(func, arg):
     return result, end_time - start_time
 
 
-# print results in a table
-def print_results_table(method_name, input_series, times):
-    print(f"{'n':<10} {'Time (seconds)':<20}")
-    print("-" * 30)
-    for n, time_taken in zip(input_series, times):
-        print(f"{n:<10} {time_taken:<20.4f}")
-    print("\n")
+# inputs
+input_1 = [5, 7, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 35, 37, 40, 42, 45]
+input_2 = [501, 631, 794, 1000, 1259, 1585, 1995, 2512, 3162, 3981, 5012, 6310, 7943, 10000, 12589, 15849]
 
+results = {method.__name__: {'values': [], 'times': []} for method in
+           [fibonacci_recursive, fibonacci_dynamic, fibonacci_backtracking, fibonacci_matrix, fibonacci_binet,
+            fibonacci_iterative]}
+
+
+for n in input_1:
+    _, time_taken = measure_time(fibonacci_recursive, n)
+    results['fibonacci_recursive']['times'].append(time_taken)
+
+for n in input_2:
+    for method in [fibonacci_dynamic, fibonacci_backtracking, fibonacci_matrix, fibonacci_binet, fibonacci_iterative]:
+        _, time_taken = measure_time(method, n)
+        results[method.__name__]['times'].append(time_taken)
+
+
+# plot all methods
+for method_name, data in results.items():
+    plt.figure(figsize=(10, 6))
+    if method_name == 'fibonacci_recursive':
+        plt.plot(input_1, data['times'], marker='o', label=method_name, color='m')
+        for n, time_taken in zip(input_1, data['times']):
+            plt.text(n, time_taken, f'{time_taken:.4f} sec', ha='right', va='bottom', fontsize=8)
+    else:
+        plt.plot(input_2, data['times'], marker='o', label=method_name, color='m')
+        for n, time_taken in zip(input_2, data['times']):
+            plt.text(n, time_taken, f'{time_taken:.4f} sec', ha='right', va='bottom', fontsize=8)
+
+    plt.title(f'Execution Time for {method_name} Method')
+    plt.xlabel('n-th Fibonacci Term')
+    plt.ylabel('Time (seconds)')
+    plt.yscale('log')
+    plt.legend()
+    plt.grid(True)
+
+plt.show()
+
+# print results for recursive
+recursive_table = [["n", "Time (seconds)"]]
+for n, time_taken in zip(input_1, results['fibonacci_recursive']['times']):
+    recursive_table.append([n, f"{time_taken:.6f}"])
+
+print("\nRecursive Method Results:")
+print(tabulate(recursive_table, headers="firstrow", tablefmt="grid"))
+
+# print results for other methods
+methods = ['fibonacci_dynamic', 'fibonacci_backtracking', 'fibonacci_matrix', 'fibonacci_binet', 'fibonacci_iterative']
+methods_table = [["n"] + methods]
+
+for i, n in enumerate(input_2):
+    row = [n] + [f"{results[m]['times'][i]:.6f}" for m in methods]
+    methods_table.append(row)
+
+print("\nOther Methods Results:")
+print(tabulate(methods_table, headers="firstrow", tablefmt="grid"))
